15552d931cb5f8d0e91b8dec79a60566021e2a77
Papatoncio <juanpij23@hotmail.com>
2024-07-29 00:57:34 -0600

    Descarga modelos, pdf, csv y generación de gráficas

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import ast
-from flask import Flask, jsonify, request, render_template, make_response
+import csv
+from flask import Flask, jsonify, request, render_template, make_response, send_file
 from flask_cors import CORS
 from supabase import create_client, Client
 from supabase.client import ClientOptions
@@ -25,6 +26,9 @@ from sklearn.decomposition import PCA
 from scipy.cluster.hierarchy import linkage, dendrogram
 from sklearn.manifold import TSNE
 from PIL import Image as imgb64
+from sklearn.preprocessing import MinMaxScaler
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 
 # * Instanciación de la app
 app = Flask(__name__)
@@ -32,7 +36,7 @@ app = Flask(__name__)
 # * Agrega soporte CORS para todos los endpoints, todos los origenes
 CORS(app)
 
-# ! Variables de entorno
+# * Variables de entorno
 load_dotenv()
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
@@ -53,17 +57,17 @@ supabase: Client = create_client(url, key,
 def index():
     return render_template('index.html')
 
-# ! Reporte PDF
-@app.route('/reporte')
+# * Reporte PDF
+@app.route('/reporte', methods=["POST"])
 def generatepdf():
     try:
-        body = {"":""}
+        body = request.get_json()
 
         # Si el body esta vació retornar todas las columnas
         if body is None or body == {}:
             return jsonify({"error": "No se encontraron valores para realizar la operación"}), 400
 
-        idmodelo = 50
+        idmodelo = body.get("id")
 
         # Verificar si se proporciono idmodelo
         if idmodelo is None or idmodelo == "":
@@ -88,32 +92,12 @@ def generatepdf():
             valor = dato.get("valor")
 
             if tipo == "1":
-                # Convertir la cadena a una lista
-                lista = ast.literal_eval(valor)
-                table_data = []
-                for i in range(0, len(lista), 2): # Manejo de arreglos de texto
-                    row = lista[i:i+2]
-                    if len(row) < 2:
-                        row.append('')  # Añadir una celda vacía si la fila tiene menos de 2 elementos
-                    table_data.append(row)
-
-                # Crear la tabla
-                table_data_paragraphs = [[Paragraph("- " + cell, styles['BodyText']) for cell in row] for row in table_data]
-
-                table = Table(table_data_paragraphs, colWidths='*')
-                table.setStyle(TableStyle([
-                    ('BACKGROUND', (0, 0), (-1, 0), colors.azure),
-                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                    ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                    ('FONTSIZE', (0, 0), (-1, 0), 12),
-                    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                    ('BACKGROUND', (0, 1), (-1, -1), colors.azure),
-                    ('GRID', (0, 0), (-1, -1), 1, colors.black),
-                ]))
-
-                story.append(table)
-                story.append(Spacer(1, 12))
+                if nombre == "Columnas Dataset":
+                    story.append(generatabla(valor, colors.azure, colors.azure))
+                    story.append(Spacer(1, 12))
+                else:
+                    story.append(generatabla(valor, colors.beige, colors.azure))
+                    story.append(Spacer(1, 12))
             elif tipo == "2":
                 img_data = base64.b64decode(valor)
                 img_buffer = BytesIO(img_data)
@@ -123,15 +107,11 @@ def generatepdf():
         doc.build(story)
         buffer.seek(0)
 
-        response = make_response(buffer.read())
-        response.headers['Content-Type'] = 'application/pdf'
-        response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
-
-        return response
+        return send_file(buffer, download_name="Reporte_modelo.pdf", as_attachment=True), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-# ! Peticiones Respuestas
+# * Peticiones Respuestas
 # * Obtener todas las respuestas
 @app.route('/respuestas')
 def getallrespuestas():
@@ -175,8 +155,24 @@ def exportcsv():
                     elif filtro.get("tipo") == "lt":
                         query = query.lt(filtro.get("columna"), filtro.get("valor"))
 
-        # Regresar datos filtrados
-        return jsonify({ "mensaje" : "Datos consultados correctamente", "rows" : str(query.csv().execute()) }), 200
+        # Ejecutar la query
+        datos = query.execute().data
+
+        # Crear un buffer en memoria para el CSV
+        buffer = io.StringIO()
+        writer = csv.writer(buffer)
+
+        # Escribir los encabezados
+        if datos:
+            writer.writerow(datos[0].keys())
+
+            # Escribir los datos
+            for row in datos:
+                writer.writerow(row.values())
+
+        buffer.seek(0)
+
+        return send_file(io.BytesIO(buffer.getvalue().encode()), mimetype='text/csv', as_attachment=True, download_name='CalmaMente_dataset.csv')
     except Exception as e:
         # Retorna el error
         return jsonify({ "error" : str(e) }), 400
@@ -191,8 +187,6 @@ def insertrespuesta():
         if body is None or body == {}:
             return jsonify({ "error" : "No se encontraron valores a insertar" }), 400
 
-        supabase.table("respuestas").insert(body).execute()
-
         # Convertir el Json a un dataframe
         df = pd.DataFrame([body])
 
@@ -202,11 +196,15 @@ def insertrespuesta():
         if modeloglobal is None:
             return jsonify({"error": "El modelo no ha sido cargado."}), 400
 
-        prediccion = modeloglobal.predict(X)[0]
+        prediccion = str(modeloglobal.predict(X)[0])
+
+        body["Nivel de Ansiedad"] = prediccion
+
+        supabase.table("respuestas").insert(body).execute()
 
         # Retorna la predicción con los datos insertados
         return jsonify({ "mensaje" : "Predicción realizada correctamante",
-                        "prediccion" : str(prediccion) }), 200
+                        "prediccion" : prediccion }), 200
     except Exception as e:
         # Retorna el error
         return jsonify({ "error" : str(e) }), 400
@@ -255,7 +253,7 @@ def insertmodelo():
         
         if tipo == "kmeans":
             modelo = KMeans(**parametros)
-        elif tipo == "gaus":
+        elif tipo == "gauss":
             modelo = GaussianMixture(**parametros)
         else:
             return jsonify({"error": "Tipo de algoritmo no soportado"}), 400
@@ -285,11 +283,11 @@ def insertmodelo():
         idmodelo = supabase.table("modelos").insert(data).execute().data[0].get("id")
 
         if tipo == "kmeans":
-            clusterskmeans(idmodelo, modelo, X)
+            clusterskmeans(idmodelo, X)
             metodocodokmeans(idmodelo, X)
-        elif tipo == "gaus":
-            # sdasd
-            print("gaus")
+        elif tipo == "gauss":
+            metodocodogaussianmixture(idmodelo, X)
+            dsads(idmodelo, X)
         else:
             return jsonify({"error": "Tipo de algoritmo no soportado"}), 400
 
@@ -298,8 +296,11 @@ def insertmodelo():
         if columnas is None or columnas == []:
             return jsonify({"error": "No se encontraron columnas"}), 400
 
-        # # Guardar columnas del dataset
+        # Guardar columnas del dataset
         columnasdataset(idmodelo, columnas)
+
+        # Guardar puntuaciones metricas de evaluación
+        datospuntuaciones(idmodelo, modelo, X, tipo)
 
         return jsonify({ "mensaje": "Modelo guardado correctamente" }), 200
     except Exception as e:
@@ -377,10 +378,40 @@ def updatemodelo():
 @app.route('/modelos/cargado')
 def checkmodelo():
     try:
-        print(modeloglobal.__getstate__())
         return jsonify({"mensaje": "Modelo actualizado correctamente", "modelo" : {
             "clase" : str(modeloglobal.__class__)
         } }), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+# * Descargar Modelo
+@app.route('/modelos/descargar', methods=["POST"])
+def descargarmodelo():
+    try:
+        body = request.get_json()
+
+        if body is None or body == {}:
+            return jsonify({"error": "No se encontraron valores para realizar la operación"}), 400
+        
+        idmodelo = body.get("id")
+
+        if idmodelo is None or idmodelo == "":
+            return jsonify({"error": "No se encontro el modelo"}), 400
+
+        # Establecer atributo principal de resto de modelos como false
+        data = supabase.table("modelos").select("modelo").eq("id", idmodelo).execute().data[0]
+        nombre = str(data.get("nombre")) + ".pkl"
+        modelo_base64 = data.get("modelo")
+
+        # Convertir de Base64 a bytes
+        modelobytes = base64.b64decode(modelo_base64)
+        
+        # Crear un archivo en memoria
+        memory_file = io.BytesIO()
+        memory_file.write(modelobytes)
+        memory_file.seek(0)
+
+        return send_file(memory_file, download_name=nombre, as_attachment=True), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
@@ -388,6 +419,46 @@ def checkmodelo():
 def columnasdataset(idmodelo, columnas):
     try:
         guardardatosreporte("Columnas Dataset", columnas, "1", idmodelo)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+def datospuntuaciones(idmodelo, modelo, x, tipo):
+    try:
+        # Escalado de datos
+        escalador = MinMaxScaler().fit(x)
+        ansiedad = pd.DataFrame(escalador.transform(x), columns=x.columns)
+
+        # Reducción de dimensionalidad antes de K-means
+        pca = PCA(n_components=2)
+        pca_ansiedad = pca.fit_transform(ansiedad)        
+        datos = []
+        datos.append("Métrica")
+        datos.append("Puntuación")
+
+        if tipo == "kmeans":
+            labels = modelo.labels_
+
+            inercia = str(modelo.inertia_)
+            datos.append("Inercia Kmeans")
+            datos.append(inercia)
+        elif tipo == "gauss":
+            # Aplicar GMM
+            gmm = GaussianMixture(n_components=4).fit(pca_ansiedad)
+            labels = gmm.predict(pca_ansiedad)
+
+        silhouette_avg = str(silhouette_score(pca_ansiedad, labels))
+        datos.append("Coeficiente de silueta")
+        datos.append(silhouette_avg)
+
+        db_score = str(davies_bouldin_score(pca_ansiedad, labels))
+        datos.append("Indice Davies-Bouldin")
+        datos.append(db_score)
+        
+        ch_score = str(calinski_harabasz_score(pca_ansiedad, labels))
+        datos.append("Indice Calinski-Harabasz")
+        datos.append(ch_score)
+
+        guardardatosreporte("Criterios de Evaluación", datos, "1", idmodelo)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
@@ -413,30 +484,96 @@ def metodocodokmeans(idmodelo, data):
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-def clusterskmeans(idmodelo, modelo, data):
+def clusterskmeans(idmodelo, x):
     try:
-        print("---")
-        # Centroides de los clusters
-        centroids = modelo.cluster_centers_
-        print("---")
-        # Etiquetas de los clusters
-        labels = modelo.labels_
-        print("---")
-        # Gráfico de dispersión con clusters
-        plt.figure(figsize=(12, 8))
-        scatter = plt.scatter(data[:, 0], data[:, 1], c=labels, s=50, cmap='viridis', alpha=0.7)
-        plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='red', marker='X', label='Centroides')
+        # Escalado de datos
+        escalador = MinMaxScaler().fit(x)
+        ansiedad = pd.DataFrame(escalador.transform(x), columns=x.columns)
+
+        # Reducción de dimensionalidad antes de K-means
+        pca = PCA(n_components=2)
+        pca_ansiedad = pca.fit_transform(ansiedad)
+
+        # Aplicar K-means
+        modelo = KMeans(n_clusters=4, n_init=10, max_iter=300).fit(pca_ansiedad)
+
+        # Grafica los resultados
+        ansiedad_pca = pd.DataFrame(pca_ansiedad, columns=['pca_one', 'pca_two'])
+        ansiedad_pca['cluster'] = modelo.labels_
+
+        plt.figure(figsize=(6, 5), dpi=100)
+        colores = ["red", "blue", "green", "black", "purple", "pink"]
+
+        for cluster in range(4):
+            plt.scatter(ansiedad_pca.loc[ansiedad_pca["cluster"] == cluster, "pca_one"],
+                        ansiedad_pca.loc[ansiedad_pca["cluster"] == cluster, "pca_two"],
+                        s=50, c=colores[cluster], label=f"Cluster {cluster}")
+            plt.scatter(modelo.cluster_centers_[cluster, 0],
+                        modelo.cluster_centers_[cluster, 1],
+                        marker="X", s=200, c="orange")
+
+        plt.title("Visualización de Clústeres utilizando PCA")
+        plt.xlabel("PCA Componente 1")
+        plt.ylabel("PCA Componente 2")
         plt.legend()
-        plt.title('Gráfico de Dispersión con Clusters')
-        plt.colorbar(scatter, label='Cluster')
-        plt.show()
-        print("---")
+
         guardardatosreporte("Clusters Kmeans", plt, "2", idmodelo)
         plt.close("all")
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
-# ! Gráficas gauss
+# * Gráficas gauss
+def metodocodogaussianmixture(idmodelo, data):
+    try:
+        # Gráfico de Elbow
+        n_components_range = range(1, 11)
+        bics = []
+        for n in n_components_range:
+            gmm = GaussianMixture(n_components=n)
+            gmm.fit(data)
+            bics.append(gmm.bic(data))
+            
+        plt.figure(figsize=(8, 6))
+        plt.plot(n_components_range, bics, 'bo-')
+        plt.xlabel('Número de componentes')
+        plt.ylabel('BIC')
+        plt.title('Gráfico de Elbow para el GMM')
+
+        guardardatosreporte("Método del Codo GaussianMixture", plt, "2", idmodelo)
+        plt.close("all")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+def dsads(idmodelo, x):
+    # Escalado de datos
+    escalador = MinMaxScaler().fit(x)
+    ansiedad = pd.DataFrame(escalador.transform(x), columns=x.columns)
+
+    # Reducción de dimensionalidad antes de K-means
+    pca = PCA(n_components=2)
+    pca_ansiedad = pca.fit_transform(ansiedad)
+    print(pca_ansiedad.shape)
+
+    # Aplicar K-means
+    modelo = GaussianMixture(n_components=4, n_init=10, max_iter=300).fit(pca_ansiedad)
+
+    # Medias de las gaussianas
+    means = modelo.means_
+
+    # Etiquetas de los clusters
+    labels = modelo.predict(pca_ansiedad)
+
+    # Gráfico de dispersión con clusters
+    plt.figure(figsize=(8, 6))
+    print(pca_ansiedad.shape)
+    scatter = plt.scatter(pca_ansiedad[:, 0], pca_ansiedad[:, 1], c=labels, s=50, cmap='viridis', alpha=0.7)
+    plt.scatter(means[:, 0], means[:, 1], s=300, c='red', marker='X', label='Medias')
+    plt.legend()
+    plt.title('Gráfico de Dispersión con Clusters')
+    plt.colorbar(scatter, label='Cluster')
+
+    guardardatosreporte("Gráfico de dispersión con clusters", plt, "2", idmodelo)
+    plt.close("all")
 
 def guardardatosreporte(nombre, dato, tipo, idmodelo):
     try:        
@@ -473,6 +610,35 @@ def guardardatosreporte(nombre, dato, tipo, idmodelo):
         return jsonify({"mensaje": "Datos guardados correctamente" }), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+    
+def generatabla(valor, bgcolor1, bgcolor2):
+    styles = getSampleStyleSheet()
+
+    # Convertir la cadena a una lista
+    lista = ast.literal_eval(valor)
+    table_data = []
+    for i in range(0, len(lista), 2): # Manejo de arreglos de texto
+        row = lista[i:i+2]
+        if len(row) < 2:
+            row.append('')  # Añadir una celda vacía si la fila tiene menos de 2 elementos
+        table_data.append(row)
+
+    # Crear la tabla
+    table_data_paragraphs = [[Paragraph(cell, styles['BodyText']) for cell in row] for row in table_data]
+
+    table = Table(table_data_paragraphs, colWidths='*')
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), bgcolor1),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), bgcolor2),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+    ]))
+
+    return table
 
 # * Ejecución del servidor (Siempre debe ir al final de la configuración)
 if __name__ == "__main__":
